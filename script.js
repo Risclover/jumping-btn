c12cfef2bddc2d7271ec52a2d591e72c8b18d5e8
Sara Dunlop <sara091592@gmail.com>
2022-08-24 13:00:44 -0700

    init commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const btn = document.getElementById("btn-1");
+const btn2 = document.getElementById("btn-2");
+const left = document.querySelector(".left");
+const right = document.querySelector(".right");
+
+btn.addEventListener("mouseenter", function (e) {
+  e.target.style.display = "none";
+  btn2.style.display = "inline-block";
+});
+
+btn2.addEventListener("mouseenter", function (e) {
+  e.target.style.display = "none";
+  btn.style.display = "inline-block";
+});
